a3db04397f855b31ba130c585af20088a0449931
Vladimir Poliakov <voledemar@inbox.ru>
2018-03-10 19:19:17 +0300

    [imp]: add copyright preamble

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
+// Copyright 2018 Shestakov Denis. All rights reserved.
+// Use of this source code is governed by a Lil
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/gocraft/web"
+	"github.com/gocraft/web"
 )
 
 type Context struct {
-
 }
 
 func (this *Context) Root(rw web.ResponseWriter, req *web.Request) {
-    fmt.Fprintf(rw, "URL = %s", req.URL)
+	fmt.Fprintf(rw, "URL = %s", req.URL)
 }
 
 func main() {
-    var ctx Context
-    router := web.New(ctx).Get("/", (*Context).Root)
-    http.ListenAndServe(":8000", router) 
+	var ctx Context
+	router := web.New(ctx).Get("/", (*Context).Root)
+	http.ListenAndServe(":8000", router)
 }
